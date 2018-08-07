fea40cdb9fcaef90444aa9b41133ad0183dfcf20
Hanna Preston <hkpreston92@bellsouth.net>
2018-08-06 21:40:17 -0400

    fixed the darn contact form issues

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// SUBMIT DISABLED BUTTON
+$(function(){
+  $('.input').keyup(function() {
+
+    var empty = false;
+    $('.input').each(function(){
+      if ($(this).val() == '') {
+        empty = true;
+      }
+    });
+
+    if (empty) {
+      $('#submit').attr('disabled', 'disabled').hasClass('.button--disabled').removeClass('button--primary');
+    } else {
+      $('#submit').removeAttr('disabled').addClass('button--primary').removeClass('button--disabled');
+    }
+  });
+})
+
+// HAMBURGER MENU
+$(function() {
+
+  $('.menu-button').on('click', function(e) {
+    $('.menu').toggleClass('menu--active');
+  });
+
+});
